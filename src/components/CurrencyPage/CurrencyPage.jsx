06b7277cefd65b707124ef6d8d0900e3bb4e8280
Kostushka <nastyatery47@yandex.ru>
2022-02-05 18:14:36 +0300

    update

--- src/components/CurrencyPage/CurrencyPage.jsx
+++ src/components/CurrencyPage/CurrencyPage.jsx
@@ -1,12 +1,30 @@
-import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+import { getApiData } from '../../utils/network';
 import styles from './CurrencyPage.module.css';
 
 const CurrencyPage = () => {
-    return <>Currency</>;
-};
+    const [currency, setCurrency] = useState([]);
 
-CurrencyPage.propTypes = {
-    text: PropTypes.string,
+    const getData = async () => {
+        const res = await getApiData();
+        setCurrency(Object.keys(res));
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+    return (
+        <>
+            <h1>Currency</h1>
+            <ul className={styles.container}>
+                {currency.map((el, i) => (
+                    <li key={i} className={styles.list}>
+                        {el}
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
 };
 
 export default CurrencyPage;
--- src/utils/network.js
+++ src/utils/network.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
-export const getApiData = async (url = 'RUB') => {
+export const getApiData = async () => {
     try {
         const res = await axios.get(
-            `http://data.fixer.io/api/latest?access_key=5cc8c14976cbf1d1cccb10076a93d127&symbols=${url}`
+            `http://data.fixer.io/api/latest?access_key=5cc8c14976cbf1d1cccb10076a93d127`
         );
-        console.log(res.data.rates);
         return res.data.rates;
     } catch (error) {
         console.error('Could not fetch', error.message);
